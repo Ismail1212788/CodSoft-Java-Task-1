1f9ace97f63d37504d201c3c7030853afe0ce030
Muhammad Ismail <muhammadismaillashari0@gmail.com>
2023-09-25 00:53:43 -0700

    initial commit

---
+++ src/main/java/com/mycompany/numbergame/NumberGame.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.numbergame;
+
+/**
+ *
+ * @author ali
+ */
+public class NumberGame {
+
+    public static void main(String[] args) {
+        PlayForm a=new PlayForm();
+        a.show();}
+}
